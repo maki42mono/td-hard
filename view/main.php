efcb6fc2a674e93c1d2e50eebc2ed8095f66eae2
Maksim Pukh <packagist@makimono.ru>
2021-04-26 22:37:56 +0300

    Попробовал одну верстку колоннами

--- view/main.php
+++ view/main.php
@@ -3,7 +3,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
 </head>
 <body>
-<h1>Максим Пух, тестовое задание для бекенд(фуллстек)-разработчиков.</h1>
+
 <style>
     .b-paginator__li > a {
         /*display: block;*/
@@ -20,15 +20,27 @@
     .b-paginator__li--active {
         background-color: gold;
     }
+
+    .b-news__item {
+        padding: 10px 0;
+    }
+
+    .b-news > .b-news__item:nth-child(odd) .b-news-item__striped {
+        background: #f5f5f5;
+    }
 </style>
-<div id="app">
-    <?php
-    include ("paginator.php");
-    include ("news-list.php");
-    include ("paginator.php");
-    include ("modal.php");
-    ?>
+<div class="container-md">
+    <h1>Максим Пух, тестовое задание для бекенд(фуллстек)-разработчиков.</h1>
+    <div id="app">
+        <?php
+        include ("paginator.php");
+        include ("news-list.php");
+        include ("paginator.php");
+        include ("modal.php");
+        ?>
+    </div>
 </div>
+
 
 
 <script src="https://cdn.jsdelivr.net/npm/vue@2.6.12/dist/vue.js"></script>
--- view/news-list.php
+++ view/news-list.php
@@ -1,16 +1,29 @@
-<ul>
-    <li v-for="newsItem in news">
-        <span v-if="newsItem.isDraft">(Ч)</span>
-        {{ newsItem.title }} {{ newsItem.publishedDate }} {{ newsItem.descriptionShort }} <br>
-        {{ newsItem.descriptionLong }}
-        <button @click="editNews(newsItem)">Изменить!</button><br>
-        <button @click="deleteNews(newsItem)">Удалить…</button><br>
-        <div v-if="newsItem.image">
-            <img style="max-height: 70px;" :src="'/src/image/' + newsItem.image">
+<div class="container b-news">
+    <div v-for="newsItem in news" class="b-news__item row">
+        <div class="row gx-5">
+            <div class="col-6 b-news-item__striped">
+                <span v-if="newsItem.isDraft">(Черн.)</span>
+                {{ newsItem.title }} (опубликована {{ newsItem.publishedDate }})<br>
+                {{ newsItem.descriptionShort }}
+            </div>
+            <div class="col-2">
+                <div class="row gy-2">
+                    <button type="button" @click="editNews(newsItem)" class="btn btn-outline-primary">Изменить</button>
+                    <br>
+                    <button type="button" @click="deleteNews(newsItem)" class="btn btn-outline-danger">Удалить</button>
+                </div>
+            </div>
         </div>
-        <br><br>
-    </li>
-</ul>
+        <div class="row">
+            {{ newsItem.descriptionLong }}
+        </div>
+        <div class="row">
+            <div v-if="newsItem.image">
+                <img style="max-height: 70px;" :src="'/src/image/' + newsItem.image">
+            </div>
+        </div>
+    </div>
+</div>
 <button @click="addNews(true)">
     Добавить и редактировать
 </button>
